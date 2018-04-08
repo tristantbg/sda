9d01b7b2e74a50f719f5bfbf72e42e6559f226fb
Tristan Bagot <tristan.bagot@gmail.com>
2018-04-08 15:58:05 +0200

    prevent post before time()

--- site/plugins/sitemap.php
+++ site/plugins/sitemap.php
@@ -9,7 +9,15 @@ kirby()->routes(array(
 		'action'  => function() use ($exclude, $important) {
 
 			$sitemap = '<?xml version="1.0" encoding="utf-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
-			foreach(site()->pages()->index()->visible() as $p){
+			foreach(site()->pages()->index()->visible()->filterBy('intendedTemplate', 'not in', ['news', 'post']) as $p){
+				if(!in_array($p->uri(), $exclude)){
+					$sitemap .= '<url><loc>' . html($p->url());
+					$sitemap .= '</loc><lastmod>' . $p->modified('c') . '</lastmod><priority>';
+					$sitemap .= ($p->isHomePage()||in_array($p->uri(), $important)) ? 1 : 0.6/$p->depth();
+					$sitemap .= '</priority></url>';
+				}
+			}
+			foreach(site()->pages()->index()->visible()->filterBy('intendedTemplate', 'in', ['news', 'post'])->filterBy('date', '<', time()) as $p){
 				if(!in_array($p->uri(), $exclude)){
 					$sitemap .= '<url><loc>' . html($p->url());
 					$sitemap .= '</loc><lastmod>' . $p->modified('c') . '</lastmod><priority>';
